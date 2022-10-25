a72de6c15821b2e1ac96e05bea479a79184d6241
Taylor Allen <taylorallen0913@gmail.com>
2022-10-25 12:39:58 -0700

    done

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ words = input('Enter the list: ').split()
 
 are = ['a', 'r', 'e']
 idxlst = []
-
+res = []
 # ******************************
 # Make your Code
 # ******************************
 
 # print the words that has 'a', 'r', 'e' in sequence
 
+for word in words:
+    idxlst = [word.find(are[j]) for j in range(len(are))]
+
+    if -1 not in idxlst:
+        res.append(word)
+
+    idxlst = []
+
+print(res)
